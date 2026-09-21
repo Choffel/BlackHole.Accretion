d76f3151691f23f0b0e780c2c1a403430237e0f9
Daniil <71185355+Choffel@users.noreply.github.com>
2026-09-21 23:30:18 +0200

    Add simulation API and SignalR wiring
    This commit configures the API host for the simulation system by registering the physics engine, simulation state, background worker, controllers, and SignalR hub. It also enables OpenAPI/Swagger UI and serves static files for the frontend.

--- src/Accretion.Api/Program.cs
+++ src/Accretion.Api/Program.cs
@@ -1,15 +1,42 @@
+using Accretion.Application.Interfaces;
+using Accretion.Application.PipLine;
+using Accretion.Application.Service;
+using Accretion.Application.Worker;
+using Accretion.Infrastructure;
+using Accretion.Infrastructure.Hub;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<SimulationStateService>();
+builder.Services.AddSingleton<IPhysicsEngine, PhysicsEngine>();
+builder.Services.AddSingleton<SimulationChannel>();
+
+builder.Services.AddHostedService<PhysicsWorker>();
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Accretion API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapHub<SimulationHub>("/hubs/simulation");
+
+app.Run();
